18f6de5436f352add0761ae28a1f555075d68ba5
sashatikhaya <aleksandrabakonina11@gmail.com>
2025-03-29 22:21:13 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install Flask==2.1.1
 pip install python-telegram-bot==13.15
-pip install werkzeug==2.0.3  # Добавляем нужную совместимую версию werkzeug
+pip install werkzeug==2.0.3
+pip install gunicorn  # Добавляем поддержку Gunicorn
