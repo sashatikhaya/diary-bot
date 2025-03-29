46dc3d1147c5483c9f03684f674cdced7b61c365
sashatikhaya <aleksandrabakonina11@gmail.com>
2025-03-29 22:18:09 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-# Установка зависимостей
 pip install Flask==2.1.1
 pip install python-telegram-bot==13.15
+pip install werkzeug==2.0.3  # Добавляем нужную совместимую версию werkzeug
